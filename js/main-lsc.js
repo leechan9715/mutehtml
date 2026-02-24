ce58617eb3fd643c2eae263afc389c58f730d994
leechan9715 <iseungchan809@gmail.com>
2026-02-24 21:58:33 +0900

    swiper 기능 추가

---
+++ js/main-lsc.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper(".lsc-swiper-2", {
+  slidesPerView: 3.5,
+  spaceBetween: 30,
+  centeredSlides: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+const swiper2 = new Swiper(".lsc-swiper-1", {
+  slidesPerView: 4.5,
+  spaceBetween: 15,
+  centeredSlides: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
