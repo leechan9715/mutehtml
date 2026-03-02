b12120c69ba879fc605afcbc6914dd86470c73ca
leechan9715 <iseungchan809@gmail.com>
2026-03-02 20:38:00 +0900

    0302작업

--- js/header.js
+++ js/header.js
@@ -4,7 +4,6 @@ function updateTime() {
   const now = new Date();
   const hour = String(now.getHours()).padStart(2, "0");
   const minute = String(now.getMinutes()).padStart(2, "0");
-  const second = String(now.getSeconds()).padStart(2, "0");
   time.textContent = `${hour}:${minute}`;
 }
 
