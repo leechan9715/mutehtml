12c45a79c63c55b43578b3ff25ada90e95867e48
zooWorldCode <0706nhj@gmail.com>
2026-03-03 18:06:14 +0900

    google add

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import SignUpInfoView from '@/views/OnboardingFlow/SignUpInfoView.vue';
 import MainLayout from '@/views/main/MainLayout.vue';
 import MainView from '@/views/main/MainView.vue';
 import ArtistSelectView from '@/views/OnboardingFlow/ArtistSelectView.vue';
+import SignUpGoogle from '@/views/OnboardingFlow/SignUpGoogle.vue';
 
 const routes = [
     {
@@ -15,7 +16,7 @@ const routes = [
         children: [
             { path: '', redirect: 'splash' },
             { path: 'splash', component: SplashView },
-            { path: 'signup', component: SignUp },
+            { path: 'signup', component: SignUpGoogle },
             { path: 'signup-info', component: SignUpInfoView },
             { path: 'welcome', component: WelcomeView },
             { path: 'artist-select', component: ArtistSelectView }
