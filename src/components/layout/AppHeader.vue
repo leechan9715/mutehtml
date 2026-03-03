d302a21ef2bdd8983cc64a8d3ace120618d0e56b
leechan9715 <iseungchan809@gmail.com>
2026-03-03 09:37:54 +0900

    AppHeader 추가

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,9 +1,97 @@
-<template></template>
+<template>
+    <header class="container">
+        <!-- 시간출력 부분 -->
+        <div class="row">
+            <div class="col-2">
+                <p class="time">{{ time }}</p>
+            </div>
+            <div class="col-2">
+                <span class="material-symbols-outlined"> android_cell_4_bar </span>
+                <span class="material-symbols-outlined"> wifi </span>
+                <span class="material-symbols-outlined"> battery_android_frame_full </span>
+            </div>
+        </div>
+        <!-- 프로필 헤더 부분 -->
+        <div class="row" v-if="isProfile">
+            <div class="col-2">
+                <img :src="logo" alt="logo" />
+            </div>
+            <div class="col-2">
+                <img :src="test" alt="test-img" />
+            </div>
+        </div>
+    </header>
+</template>
 
 <script>
+import clockMixin from '@/mixins/clockMixin';
+import logo from '@/assets/images/common/logo.png';
+import test from '@/assets/images/common/test.jpg';
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
+    mixins: [clockMixin],
+    props: {
+        isProfile: {
+            type: Boolean,
+            required: true,
+            default: false
+        }
+    },
+    data() {
+        return {
+            logo,
+            test
+        };
+    }
 };
 </script>
 
-<style></style>
+<style scoped>
+header {
+    position: relative;
+    z-index: 9999;
+}
+header.container {
+    border-radius: 0 0 20px 20px;
+    /* border: 1px solid var(--color-primary-4); */
+    padding: 13px 25px;
+    gap: 23px;
+    background-color: #fff;
+}
+
+header .row:nth-child(1) {
+    justify-content: space-between;
+}
+
+header .row:nth-child(1) .col-2:nth-child(2) {
+    display: flex;
+    gap: 7px;
+}
+
+header .row:nth-child(2) {
+    justify-content: space-between;
+    align-items: center;
+}
+
+header .row:nth-child(2) .col-2:nth-child(1) {
+    max-width: 90px;
+}
+
+header .row:nth-child(2) .col-2:nth-child(1) img {
+    width: 100%;
+}
+
+header .row:nth-child(2) .col-2:nth-child(2) {
+    border-radius: 12px;
+    width: 42px;
+}
+
+header .row:nth-child(2) .col-2:nth-child(2) img {
+    object-fit: cover;
+    border-radius: 12px;
+}
+
+header .row .col-2 .time {
+    font-weight: 600;
+}
+</style>
---
+++ src/views/main/MainView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container"><h1>메인뷰</h1></div>
+</template>
+
+<script>
+export default {
+    name: 'MainView'
+};
+</script>
+
+<style></style>
