a1e65c40af0d32c9f57b5f757c98a0f23b6ae75e
leechan9715 <iseungchan809@gmail.com>
2026-03-03 19:43:01 +0900

    router이전값으로 수정

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,9 @@ import OnboardingLayout from '@/views/OnboardingFlow/OnboardingLayout.vue';
 import SplashView from '@/views/OnboardingFlow/SplashView.vue';
 import SignUp from '@/views/OnboardingFlow/SignUpView.vue';
 import WelcomeView from '@/views/OnboardingFlow/WelcomeView.vue';
-import SignUpInfoView from '@/views/OnboardingFlow/SignUpInfoView.vue';
 import MainLayout from '@/views/main/MainLayout.vue';
 import MainView from '@/views/main/MainView.vue';
 import ArtistSelectView from '@/views/OnboardingFlow/ArtistSelectView.vue';
-import SignUpGoogle from '@/views/OnboardingFlow/SignUpGoogle.vue';
 import SignUpNaverView from '@/views/OnboardingFlow/SignUpNaverView.vue';
 
 const routes = [
@@ -18,7 +16,7 @@ const routes = [
             { path: '', redirect: 'splash' },
             { path: 'splash', component: SplashView },
             { path: 'signup', component: SignUpNaverView },
-            { path: 'signup-info', component: SignUpInfoView },
+            { path: 'signup-info', component: SignUp },
             { path: 'welcome', component: WelcomeView },
             { path: 'artist-select', component: ArtistSelectView }
         ]
