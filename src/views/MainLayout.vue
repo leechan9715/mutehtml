d0a9933e5c8da97049973dcad6c74ba790854417
leechan9715 <iseungchan809@gmail.com>
2026-03-04 12:43:54 +0900

    MainLayout 경로 수정

---
+++ src/views/MainLayout.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="wrap">
+        <AppHeader :isProfile="isProfile" />
+        <main>
+            <router-view />
+        </main>
+        <AppFooter />
+    </div>
+</template>
+
+<script>
+import AppFooter from '@/components/layout/AppFooter.vue';
+import AppHeader from '@/components/layout/AppHeader.vue';
+import playlistInfo from '@/components/ui/playlist-info.vue';
+import VibeSelectBtn from '@/components/ui/VibeSelectBtn.vue';
+
+export default {
+    name: 'MainLayout',
+    components: {
+        AppHeader,
+        AppFooter,
+        playlistInfo,
+        VibeSelectBtn
+    },
+    computed: {
+        isProfile() {
+            return this.$route.matched.some((r) => r.meta?.isProfile === true);
+        }
+    }
+};
+</script>
+
+<style scoped>
+#wrap {
+    padding: 0;
+}
+main {
+    height: calc(100vh - 130px);
+    overflow: hidden;
+    overflow-y: auto;
+    scrollbar-width: none;
+}
+</style>
