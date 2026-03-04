460f89f57d4e460443747f556e63cd46e7b4f2a7
zooWorldCode <0706nhj@gmail.com>
2026-03-04 16:45:04 +0900

    save

---
+++ src/views/main/MainSec04.vue
@@ -0,0 +1,7 @@
+<template>
+    <p>작업공간 MainView에 다 모아놨어요</p>
+</template>
+
+<script></script>
+
+<style scoped src="@/assets/styles/pages/main-css/sec-04.css"></style>
--- src/views/main/MainView.vue
+++ src/views/main/MainView.vue
@@ -2,18 +2,21 @@
     <MainSec01 />
     <MainSec02 />
     <MainSec03 />
+    <MainSec04 />
 </template>
 
 <script>
 import MainSec01 from '@/views/main/MainSec01.vue';
 import MainSec02 from '@/views/main/MainSec02.vue';
 import MainSec03 from '@/views/main/MainSec03.vue';
+import MainSec04 from '@/views/main/MainSec04.vue';
 export default {
     name: 'MainView',
     components: {
         MainSec01,
         MainSec02,
-        MainSec03
+        MainSec03,
+        MainSec04
     }
 };
 </script>
