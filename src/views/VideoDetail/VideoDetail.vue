99fdf6c3e0f91bf9a81747b7da3a0a2796ea9123
leechan9715 <iseungchan809@gmail.com>
2026-03-06 15:11:22 +0900

    페이지수정작업

---
+++ src/views/VideoDetail/VideoDetail.vue
@@ -0,0 +1,10 @@
+<template>aa</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+const route = useRouter();
+const id = route.params.id;
+console.log(id);
+</script>
+
+<style></style>
