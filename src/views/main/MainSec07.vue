450b197fff55efdf510848537f46f08f2608740e
leechan9715 <iseungchan809@gmail.com>
2026-03-13 17:33:14 +0900

    fix : 쇼츠 슬라이드 갯수 수정

--- src/views/main/MainSec07.vue
+++ src/views/main/MainSec07.vue
@@ -1,7 +1,7 @@
 <template>
     <div style="padding: 16px">
         <MainContainer title="아티스트 모먼트">
-            <Swiper class="song-swiper" :slides-per-view="3.2" :space-between="16">
+            <Swiper class="song-swiper" :slides-per-view="2.5" :space-between="16">
                 <SwiperSlide v-for="post in posts" :key="post.id" class="shorts" @click="linkTo(post.id)">
                     <video
                         style="object-fit: cover"
