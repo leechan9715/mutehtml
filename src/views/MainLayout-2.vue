cf57c1a1cd06b6bf64c8932c4d192bceb369ce54
leechan9715 <iseungchan809@gmail.com>
2026-03-14 17:55:38 +0900

    feat : 공용 헤더 로컬로그인 : 기본프로필 : 소셜로그인 : 해당프로필

--- src/views/MainLayout-2.vue
+++ src/views/MainLayout-2.vue
@@ -22,13 +22,15 @@ import AppFooter from '@/components/layout/AppFooter.vue';
 import AppHeader from '@/components/layout/AppHeader-2.vue';
 import playlistInfo from '@/components/ui/playlist-info.vue';
 import VibeSelectBtn from '@/components/ui/VibeSelectBtn.vue';
+import { checkAuthApi } from '@/api/_auth_api';
 
 export default {
     name: 'MainLayout2',
     data() {
         return {
             loadingDotsData: JSON.stringify(loadingDots),
-            store: useIsLoadingStore()
+            store: useIsLoadingStore(),
+            authData: null
         };
     },
     components: {
@@ -48,6 +50,7 @@ export default {
     },
     mounted() {
         // this.loginCheck();
+        this.checkAuthData();
     },
     methods: {
         loginCheck() {
@@ -56,6 +59,11 @@ export default {
                 alert('로그인을 해주세요');
                 this.$router.push('/');
             }
+        },
+        async checkAuthData() {
+            const { data } = await checkAuthApi();
+            this.authData = data;
+            console.log('로그인한 데이터 ', data);
         }
     }
 };
